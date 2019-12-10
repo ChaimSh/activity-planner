5719589d346549d9993dca3601181bda2a1c2c3e
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-12-09 22:43:43 -0600

    add logout to controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+
+
+
+
+
 end
