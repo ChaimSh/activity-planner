86c9c98d5c43cb278dde7ded131006437247ef79
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-12-09 22:22:46 -0600

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :locations
     has_many :located_activities, through: :locations, source: :activities 
     has_many :activities
+    has_secure_password
+
+    validates :username, :email, :password, presence: true
+    validates :username, :email, uniquieness: true
+
 end
