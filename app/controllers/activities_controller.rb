f8b8e147bfe457514719fb63b88b436713b3ea9d
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-12-10 20:12:31 -0600

    created current user

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,41 @@
 class ActivitiesController < ApplicationController
+before_action :set_activity, only: [:show]
+    
+  def index
+    @activities = Activity.all
+  end
+
+  def show
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = current_user.locations.build(activity_params)
+    if @activity.save
+        redirect_to activity_path(@activity)
+    else
+        render :new
+    end
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :description, :duration)
+  end
+
+  def set_activity
+    @activity = Activity.find_by(id: params[:id])
+    redirect_to activities_path if !@activity
+  end
+
+
+
+
+
+
+
 end
