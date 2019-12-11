82684f2ccd3cdf45893a032b2b7201eb82ab9085
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-12-11 13:27:17 -0600

    add location views

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,43 @@
 class LocationsController < ApplicationController
+
+  def index
+    @location = current_user.locations
+  end
+
+  def show
+    @location = Location.find_by(id: params[:id])
+  end
+
+  def new
+    @location = Location.new
+  end
+  
+  def create
+    @location = current_user.locations.build(location_params)
+    if @location.save
+        redirect_to location_path(@location)
+    else
+        render :new
+    end
+  end
+
+  def edit
+    @location = Location.find_by(id: params[:id])
+  end
+
+  def update
+    @location = Location.find_by(id: params[:id])
+    if @location.update(location_params)
+        redirect_to location_path(@location)
+    else
+        render :edit
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :address)
+  end
+  
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,27 @@
 class Event < ApplicationRecord
     belongs_to :location
     belongs_to :activity
+
+
+    validates :date, presence: true
+    validates_associated :activity
+
+    def activity_attributes=(activity_params)
+      activity = Activty.find_or_create_by(activity_params)
+      activity_params[:name].empty? ? self.activity : self.activity = activity 
+    end 
+    
+    
+    
+    def datetime
+        self.date.strftime("%A, %b %d") if self.date
+    end
+
+
+
+
+
+
+
+
 end
