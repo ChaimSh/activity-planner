2fc902dda2ecb93d9a6ebed1ac8e664cb6f503e0
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-12-15 12:25:44 -0600

    add events helper method

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,18 @@
 module EventsHelper
 
+   def display_index_header
+     if @activity 
+        content_tag(:h2, "Events with #{@activity.name}")
+     elsif @location
+        content_tag(:h2, "Events at #{@location.name}")
+     else
+        content_tag(:h1, "All Events")
+     end
+   end
+
+
+
+
     def activity_form_option(form_builder)
         if @activity
            content_tag(:p, "Activity: #{@activity.name}")
