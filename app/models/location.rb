a1141fb2124810790df2a0626fca7a52c12c7435
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-12-18 12:12:23 -0600

    add method to index page

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,6 +6,12 @@ class Location < ApplicationRecord
   validates :name, :address, presence: :true
   validates_uniqueness_of :name, :address, scope: :user_id 
  
-  scope :order_reverse, -> {order(name: :desc).first(3)}
+  scope :order_reverse, -> {order(name: :desc).first(4)}
   
+  # def self.ordered_reverse
+  #   a = self.order_reverse 
+  #   a.each do |aa|
+  #     aa.name
+  #   end
+  # end
 end
