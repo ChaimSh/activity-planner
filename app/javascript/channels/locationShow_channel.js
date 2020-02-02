4d69f85b068826a9f2f5f04c95003db0e7a9956f
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-02-02 16:16:25 -0600

    getLocation works

--- app/javascript/channels/locationShow_channel.js
+++ app/javascript/channels/locationShow_channel.js
@@ -4,23 +4,53 @@ $(document).ready(function () {
 })
 
 function locationListener() {
-  $('a.location-data').on('click', function(e) {
-      e.preventDefault();
       getLocation()
-  })
 }
 
+
 function getLocation() {
+    var array = []
     var id = parseInt($("a.location-data").attr("data-id")) + 1;
-  fetch('http://localhost:3000/locations/' + id + '.json')
-  .then(function(data) { 
-     return Object.keys(data).map(location => {
-          const newLocation = new Location(location)
-          const newLocationHtml = newLocation.locationHTML()
-          document.getElementById('location-div').innerHTML = newLocationHtml
-      })
-  }) 
+    $.ajax({
+      url: 'http://localhost:3000/locations/' + id,
+      method: 'get',
+      dataType: 'json',
+      success: function (data) {
+            array.push(data)
+            array.map(location => {
+            const newLocation = new Location(location)
+            const newLocationHtml = newLocation.locationHTML()
+            $('a.location-data').on('click', function(e) {
+                e.preventDefault();
+            document.getElementById('location-div').innerHTML = newLocationHtml
+            $(".location-data").attr("data-id", location.id);
+            getLocation()
+          }) 
+        })
+      }
+    })
 }
+
+
+// function getLocation() {
+//        var array = []
+//     var id = parseInt($("a.location-data").attr("data-id")) + 1;
+//   fetch('http://localhost:3000/locations/' + id + '.json')
+//   .then(function(data) { 
+//          array.push(data)
+//           array.map(location => {
+//           const newLocation = new Location(location)
+//           const newLocationHtml = newLocation.locationHTML()
+//              $('a.location-data').on('click', function(e) {
+//              e.preventDefault();
+//           document.getElementById('location-div').innerHTML = newLocationHtml
+//       })
+//     }) 
+//   }) 
+// }
+
+
+
 
 class Location {
     constructor(obj) {
@@ -33,8 +63,8 @@ class Location {
 Location.prototype.locationHTML = function () {
     return (`
     <div class = location>
-    <h3>${this.name}</h3>
-    <h5>${this.address}</h5>
+    <h1>${this.name}</h1>
+    <h2>${this.address}</h2>
     </div>
     `)
 }
