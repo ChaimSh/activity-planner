55d96459ddbea7cfa0f63361e896a821102acb5e
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-02-02 22:19:20 -0600

    progress on creating new location

--- app/javascript/channels/locationForm_channel.js
+++ app/javascript/channels/locationForm_channel.js
@@ -1,23 +1,42 @@
 $(document).ready(function () {
-   listenForNewLocationFormClick()
+    newLocationFormListener()
+    createNewLocation()
     // editFormListener()
 })
 
 
-function listenForNewLocationFormClick() {
+function newLocationFormListener() {
   $('button#ajax-new-location').on('click', function (event) {
-      event.preventDefault()
-      let newLocationForm = Location.newLocationForm()
+    event.preventDefault()
+    let newLocationForm = Location.newLocationForm()
       document.querySelector('div#new-location-form-div').innerHTML = newLocationForm
   })
 }
 
+
+function createNewLocation() {
+      $(document).on('submit', 'form#newLocationForm', function(e){
+      var values = $(this).serialize();
+      var posting =  $.post('/locations', values);
+      posting.done(function(data) {
+      var location = new Location(data);
+      })
+    })
+}
+
 class Location {
-  static newLocationForm() {
-    return (`
-		<strong>New Location Form</strong>
-			<form>
+  constructor(obj) {
+    this.id = obj.id
+    this.name = obj.name
+    this.address = obj.address 
+  }
+   static newLocationForm() {
+     return (`
+		  <strong>New Location Form</strong>
+      <form>
+        Location Name:
 				<input id='Location-name' type='text' name='name'></input><br>
+        Location Address:
         <input type='text' name='address'></input><br>
 				<input type='submit' />
 			</form>
