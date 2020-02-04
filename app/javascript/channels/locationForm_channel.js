a8e0db56a392289c6b33e1dd8db9ac645c0ad586
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-02-03 21:07:43 -0600

    new attempt on creating new location

--- app/javascript/channels/locationForm_channel.js
+++ app/javascript/channels/locationForm_channel.js
@@ -1,30 +1,54 @@
-$(document).ready(function () {
-    newLocationFormListener()
-    createNewLocation()
-    // editFormListener()
-})
+// $(document).ready(function () {
+//     newLocationFormListener()
+//     createNewLocation()
+//     // editFormListener()
+// })
 
 
-function newLocationFormListener() {
-  $('button#ajax-new-location').on('click', function (event) {
-    event.preventDefault()
-    let newLocationForm = Location.newLocationForm()
-      document.querySelector('div#new-location-form-div').innerHTML = newLocationForm
-  })
-}
+// function newLocationFormListener() {
+//   $('button#ajax-new-location').on('click', function (event) {
+//     event.preventDefault()
+//     let newLocationForm = Location.newLocationForm()
+//       document.querySelector('div#new-location-form-div').innerHTML = newLocationForm
+//   })
+// }
 
+ 
+// function createNewLocation() {
+//       $('form#new-location-form').on('submit', function(e) {
+//       e.preventDefault();
+//       var values = $(this).serialize();
+//       var posting =  $.post('/locations', values);
+//       posting.done(function(data) {
+//       var location = new Location(data);
+//       var response = location.buildLocation({skipIndexLink: true});
+//       $('div#new-locations').html(response);
+//       })
+//     })
+// }
 
-function createNewLocation() {
-      $(document).on('submit', 'form#newLocationForm', function(e) {
-      var values = $(this).serialize();
-      var posting =  $.post('/locations', values);
-      posting.done(function(data) {
-      var location = new Location(data);
-      var response = location.buildLocation({skipIndexLink: true});
-    $('div#main').html(response);
-      })
-    })
-}
+$(() => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const values = $(e.target).serialize();
+    console.log('values :', values);
+    const postResp = $.post('/locations', values);
+    postResp.done((respData) => {
+    var location = new Location(respData);
+    const newHtml = location.newLocationHTML();
+    $('#js-location-info')
+    .html(newHtml)
+    .addClass('open-up');
+    }).error((respData) => {
+      let errorMessage = 'There was an error: Title ';
+      errorMessage += respData.responseJSON.title['0'];
+      $('#js-location-info').html(errorMessage)
+          .addClass('open-up-error');
+    });
+  };
+});
+$('form').submit(handleSubmit);
+
 
 class Location {
   constructor(obj) {
@@ -32,19 +56,30 @@ class Location {
     this.name = obj.name
     this.address = obj.address 
   }
-   static newLocationForm() {
-     return (`
-		  <strong>New Location Form</strong>
-      <form>
-        Location Name:
-				<input id='Location-name' type='text' name='name'></input><br>
-        Location Address:
-        <input type='text' name='address'></input><br>
-				<input type='submit' />
-			</form>
-		`)
-  }
 }
+
+Location.prototype.newLocationHTML = function () {
+  return (`
+  <div class='location'>
+  <h3>${this.name}</h3>
+  <h5>${this.address}</h5>
+  </div>
+  `)
+}
+
+//    static newLocationForm() {
+//      return (`
+// 		  <strong>New Location Form</strong>
+//       <form id='new-location-form'>
+//         Location Name:
+// 				<input id='Location-name' type='text' name='name'></input><br>
+//         Location Address:
+//         <input type='text' name='address'></input><br>
+// 				<input type='submit' />
+// 			</form>
+// 		`)
+//   }
+// }
 
 
 
